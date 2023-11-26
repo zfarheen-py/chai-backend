6e97c9021d795e5876a319e83829ffd64179f93f
Zainab Farheen <farheenzainab1104@gmail.com>
2023-11-26 08:53:39 +0530

    add register controller & bug fixes

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -57,7 +57,7 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   // Hash the password using bcrypt with a cost factor of 10
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
